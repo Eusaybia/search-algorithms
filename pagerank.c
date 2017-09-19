808c07dfd6ba8f870c194d5fc2c14da85cd291da
Kongwei Ying <yingkongwei@gmail.com>
2017-09-19 13:21:05 +1000

    Remove non-functioning statements from main

--- pagerank.c
+++ pagerank.c
@@ -11,16 +11,6 @@ int main(int argc, char *argv[]) {
 	// TODO: Read web pages from the collection file and build graph
 	// TODO: Commit to VCS system
 	// TODO: Sum of all pages' PR = 1
-	FILE *collection_fp = fopen("Sample1/collection.txt", "r");
-	Graph g = newGraph(MAX_V);
-	char url[MAX_CHAR] = {0};
-	while (fscanf(collection_fp, "%s", url) != 0) {
-		char subdir[MAX_CHAR];
-		strcpy(subdir, "Sample1/");
-		strcat(url, ".txt");
-		strcat(subdir, url);
-		FILE *url_fp = fopen(subdir, "w");
-	}
 
 	double damp = atof(argv[1]);
 	double diffPR = atof(argv[2]);
